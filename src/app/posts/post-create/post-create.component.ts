0fa6c44a64df76ddbfcac6d38f6f0feb4f5c8ff9
DESKTOP-QJEM3AF\USER <l4371alk@gmail.com>
2023-10-29 21:45:03 +0800

    Adding Forms

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,27 +1,32 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { Post } from '../post.interface';
 
-const materialGroup = [MatInputModule, MatCardModule, MatButtonModule];
+const materialGroup = [MatInputModule, MatCardModule, MatButtonModule,MatFormFieldModule];
 @Component({
   selector: 'app-post-create',
   standalone: true,
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css'],
-  imports: [FormsModule, materialGroup],
+  imports: [FormsModule, NgIf, materialGroup],
 })
 export class PostCreateComponent {
   enteredContent = '';
   enteredTitle = '';
   @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
+  onAddPost(form: NgForm) {
+    if(form.invalid){
+      return;
+    }
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent,
+      title: form.value.title,
+      content: form.value.content,
     };
     this.postCreated.emit(post);
   }
